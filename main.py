db874bd7f7f8d6c231e8e0f86dcc62ff3be6aede
kballester <kevin.ballester@sortis.es>
2026-06-30 10:29:51 +0200

    update

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
+from typing import Literal
 import cv2
 from time import perf_counter, sleep
-from utils import preprocess_image, get_tflite
-from collections import deque, defaultdict
+from utils import preprocess_image, get_tflite, top_k_predictions, group_full
+from collections import deque
 import numpy as np
 from camera import Camera
+from model import TFLiteModel
 
 
 BUFFER_LENGTH = 3
@@ -12,53 +14,31 @@ TOP_K = 5
 CONFIDENCE = 0.95
 ALLOWED_FAMILIES = ["carton", "plastic", "metal"]
 
-# --- Load TFLite model ---
-tflite = get_tflite()
-interpreter = tflite.Interpreter(model_path="model/trash_classification_v3_float32.tflite")
-input_details = interpreter.get_input_details()
-interpreter.resize_tensor_input(
-    input_details[0]["index"],
-    [BUFFER_LENGTH, 224, 224, 3]
-)
 
-output_details = interpreter.get_output_details()
-interpreter.allocate_tensors()
 
-with open("model/class_names.txt", "r") as f:
+MODEL_PATH = "models/mobilenetV3_trash_classifier/model.tflite"
+CLASS_NAMES_PATH = "models/mobilenetV3_trash_classifier/class_names.txt"
+
+MODEL_PATH = "models/mobilenetV3_trash_classifier_V2/model.tflite"
+CLASS_NAMES_PATH = "models/mobilenetV3_trash_classifier_V2/class_names.txt"
+
+DECODE_LEVEL: Literal["class", "garbage"] = "class"
+CAMERA_RESOLUTION = (720, 720)
+
+
+with open(CLASS_NAMES_PATH, "r") as f:
     class_names = [line.strip() for line in f.readlines()]
     class_to_family = {c: c.split("_")[0] for c in class_names if c.split("_")[0] in ALLOWED_FAMILIES}
-    
+
+# --- MODEL ---
+tflite_model = TFLiteModel(MODEL_PATH, CLASS_NAMES_PATH, BUFFER_LENGTH)
 
 # --- BUFFER ---
 buffer = deque(maxlen=BUFFER_LENGTH)
 
-
 # --- Camera ---
-cam = Camera(use_opencv=False)
+cam = Camera(resolution=CAMERA_RESOLUTION, use_opencv=True, show=True)
 
-def top_k_predictions(pred, class_names, k=3):
-    idx = np.argsort(pred)[::-1][:k]
-    return [(class_names[i], float(pred[i])) for i in idx]
-
-def group_prediction(top_predictions):
-    grouped = defaultdict(float)
-
-    for label, prob in top_predictions:
-        family = label.split("_")[0]
-        grouped[family] += prob
-
-    return grouped
-
-def group_full(pred, class_names, class_to_family: dict):
-    grouped = defaultdict(float)
-
-    for p, c in zip(pred, class_names):
-        family = class_to_family.get(c, None)
-        if family is None:
-            continue
-        grouped[class_to_family[c]] += float(p)
-
-    return grouped
 
 try:
     while True:
@@ -69,22 +49,21 @@ try:
         img = preprocess_image(frame, is_batch=False) #shape (224,224,3)
         buffer.append(img)
         if len(buffer) == buffer.maxlen:
-            batch = np.stack(buffer, axis=0)  # (3,224,224,3)
-            interpreter.set_tensor(input_details[0]["index"], batch)
-            interpreter.invoke()
-
-            output = interpreter.get_tensor(output_details[0]["index"])
-
-            pred_mean = output.mean(axis=0)
-            grouped_predictions = group_full(pred_mean, class_names, class_to_family)
-            print(grouped_predictions)
-            best_class = max(grouped_predictions, key=grouped_predictions.get)
+            batch = np.stack(buffer, axis=0)  #(3,224,224,3)
+            output = tflite_model.invoke(batch) #(3, 1, class_names)
+            pred_mean = output.mean(axis=0, keepdims=True) #(1, 1, class_names)
             
-            if grouped_predictions[best_class] >= CONFIDENCE:
-                print(f"{best_class} {grouped_predictions[best_class] * 100:.4}%")
+            if DECODE_LEVEL == "class":
+                predictions = tflite_model.get_top_predictions(pred_mean, 5)[0]
+            if DECODE_LEVEL == "garbage":
+                predictions = group_full(pred_mean[0], class_names, class_to_family)
+            
+            print(predictions)
+            best_class = max(predictions, key=predictions.get)
+            if predictions[best_class] >= CONFIDENCE and best_class != "person":
+                print(f"{best_class} {predictions[best_class] * 100:.4}%")
                 buffer.clear()
                 input("Pulsar boton para continuar.")
-
 
 
 except Exception as exc:
